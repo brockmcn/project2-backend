37f507bff9de006adbd57c8a21f2dc86cdcf5459
Brock <brockmcn@gmail.com>
2020-12-09 17:57:10 -0500

    mysql to app.js

--- trip-server/app.js
+++ trip-server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const fs = require('fs');
 
 const app = express();
-const port = 3000;
+app.use(cors());
+app.use(express.json());
 
+const port = 3000;
 app.listen(port, () => {
     console.log(`We're live on port ${port}!`);
 })
