cb1d77642b5b45df30a052e425025abc0659e526
Brock <brockmcn@gmail.com>
2020-12-09 19:28:16 -0500

    mysql changes with credentials

--- trip-server/app.js
+++ trip-server/app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+let connection = mysql.createConnection(credentials);
+connection.connect();
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`We're live on port ${port}!`);
